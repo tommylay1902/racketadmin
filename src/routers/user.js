d2ac2a3bb6e807e56d540d969064449a7c3bc8a5
tommylay1902 <tommylay.c@gmail.com>
2021-03-19 20:42:59 -0700

    updates

--- src/routers/user.js
+++ src/routers/user.js
@@ -15,8 +15,7 @@ router.post("/users", async (req, res) => {
         const token = await user.generateAuthToken();
         res.send({ user, token });
     } catch (e) {
-        res.send(e);
-        // res.status(400).send();
+        res.status(400).send(e);
     }
 });
 
@@ -26,7 +25,7 @@ router.post("/users/login", async (req, res) => {
         const token = await user.generateAuthToken();
         res.send({ user, token });
     } catch (e) {
-        res.status(400).send();
+        res.status(400).send(e);
     }
 });
 
